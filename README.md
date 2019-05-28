eaa08cfbb781b06378bf1fb8e3242617422eca44
afeiship <1290657123@qq.com>
2019-05-28 16:35:34 +0800

    add: initl

---
+++ README.md
@@ -0,0 +1,22 @@
+# next-media-time
+> Media like audio/video display time
+
+## installation
+```bash
+npm install -S afeiship/next-media-time --registry=https://registry.npm.taobao.org
+```
+
+## apis
+| api | params | description   |
+|-----|--------|---------------|
+| get | -      | desc balabala |
+
+## usage
+```js
+import NxMediaTime from 'next-media-time';
+
+// code goes here:
+```
+
+## resources
+- https://github.com/afeiship
---
+++ src/next-media-time.js
@@ -0,0 +1,26 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+  var nxPadStart = nx.padStart || require('next-pad-start');
+  var nxTimeformat = nx.timeFormat || require('next-time-format');
+  var DEFAULT_OPTIONS = { default: '00:00' };
+
+  var NxMediaTime = nx.declare('nx.MediaTime', {
+    statics: {
+      format: function(inTime, inOptions) {
+        var options = nx.mix(null, DEFAULT_OPTIONS, inOptons);
+        if (inTime) {
+          var formatTime = nxTimeformat(inTime * 1e3);
+          var _minute = nxPadStart(formatTime.minute, 2, '0');
+          var _second = nxPadStart(formatTime.second).padStart(2, '0');
+          return _minute + ':' + _second;
+        }
+        return options.default;
+      }
+    }
+  });
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = NxMediaTime;
+  }
+})();
